8b4dea7787a1b36f6e2fddd76463f246385b6a2c
rajatkhanna15 <rajat.khanna_cs18@gla.ac.in>
2020-03-08 17:10:09 +0530

    end of main() method.

--- src/Main.java
+++ src/Main.java
@@ -10,15 +10,30 @@ public class Main {
              */
 
             final Scanner scanner = new Scanner(System.in);
+            /*
+            Entering the number of LEDs.(the length of LED strip.)
+             */
             int numberOfLEDs = Integer.parseInt(scanner.nextLine());
+            /*
+            Creating an array of LED type with the length equal to no. of LEDs.
+             */
             LED[] array = new LED[numberOfLEDs];
+            /*
+            traversing the array and entering the required values.
+             */
             for (int i = 0; i < array.length; i++) {
                 String colourName = scanner.next();
                 boolean working = scanner.nextBoolean();
                 array[i] = new LED(colourName, working);
             }
+            /*
+            creating object of LEDstrip class.
+             */
+
             LEDStrip strip = new LEDStrip(array);
+
             strip.printStripStatus();
             scanner.close();
         }
+
 }
